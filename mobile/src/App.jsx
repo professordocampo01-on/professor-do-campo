9b9bac27f12dcce10fccebe2d2996f4ea21d0874
professordocampo01-on <professordocampo01@gmail.com>
2025-11-08 01:58:53 -0300

    add api.js to connect mobile with backend

--- mobile/src/App.jsx
+++ mobile/src/App.jsx
@@ -1,26 +1,11 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from './screens/HomeScreen'
-import MatchScreen from './screens/MatchScreen'
-import ChatScreen from './screens/ChatScreen'
-import { colors } from './theme/colors'
+// mobile/src/api.js
+import axios from "axios";
 
-const Stack = createNativeStackNavigator()
+const API_BASE_URL = "https://professor-do-campo-backend.onrender.com";
 
-export default function App(){
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: colors.background },
-          headerTintColor: colors.gold,
-          contentStyle: { backgroundColor: colors.background }
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Professor Do Campo'}} />
-        <Stack.Screen name="Match" component={MatchScreen} options={{title: 'Partida'}}/>
-        <Stack.Screen name="Chat" component={ChatScreen} options={{title: 'Chat'}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
